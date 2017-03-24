be8fe72b6dac75b08603986a55789608929cef1c
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-03-24 17:48:39 -0500

    sets up basic html, css, and document ready

---
+++ server/public/scripts/client.js
@@ -0,0 +1,11 @@
+//globals
+
+//document ready
+$(document).ready(function(){
+  console.log("We are here for you too!");
+  console.log("You're going to do great!");
+
+
+});//ends document ready
+
+//functions function are arranged alphabetically
