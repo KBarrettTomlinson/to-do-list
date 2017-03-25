4e035a96048ba326d32a6beeaea107c5641f0759
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-03-24 22:03:45 -0500

    completes appendDisplay function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -37,32 +37,52 @@ function deleteTask(){
 
 function displayData(dataArray){
   console.log("inside displayData");
-  //this function appends the dataArray of objects to the DOM
-  //while apending to the DOM it crates buttons for Completed and Delete
-  //When those buttons get created data is attached to the buttons for future
-  //reference
+
   $('#outputDiv').empty();
   for (var i = 0; i < dataArray.length; i++){
+
+    //grabs the object from the array
     var currentObject = dataArray[i];
+
     //picks apart the object
     var id= currentObject.id;
     var task = currentObject.task;
     var priority = parseInt(currentObject.priority);
     var next = currentObject.next_step;
     var completed = currentObject.completed;
+
+    //completed button text
+    var completeButtonText;
+    if (completed ===true){
+      completeButtonText = "Complete";
+    }//end if
+    else{
+      completeButtonText = "TO DO";
+    }//end else
+
     //appends dataRowDiv to the outputDiv
     $('#outputDiv').append('<div class = "dataRowDiv"></div>');
     var $el = $('#outputDiv').children().last();
-    //appends object to the rows
+
+    //appends object to the rows as a table
     $el.append('<td class="table-item">'+task+'</td>');
     $el.append('<td class="table-item">'+priority+'</td>');
     $el.append('<td class="table-item">'+next+'</td>');
 
-    //break this out into two different appends, that way it will be easier to attach data to the buttons
-    $el.append('<td class="table-button"><button class="completeButton">'+completed+'</button></td>');
-    $el.append('<td class="table-button"><button class="deleteButton">Delete</button></td>');
-    //adds data to buttons
+    //appends button to the row and adds data tags
+    $el.append('<td class="table-button"></td>');
+    var $el1 = $el.children().last();
+    $el1.append('<button class="completeButton">'+completeButtonText+'</button>');
+    var $el2 = $el1.children().last();
+    $el2.data('id',id);
+    $el2.data('completed',completed);
 
+    //appends button to the row and adds data tags
+    $el.append('<td class="table-button"></td>');
+    var $el3 = $el.children().last();
+    $el3.append('<button class="deleteButton">Delete</button>');
+    var $el4 = $el3.children().last();
+    $el4.data('id',id);
   }//ends for loop that appends to DOM
 }//ends displayData
 
@@ -71,7 +91,11 @@ function eventListeners(){
   //on submit event listener calls addTask() and passes var $this to it
   addTask();
   //on click complete calls completeTask() and passes var $this to it
-  completeTask();
+  $('#outputDiv').on('click','.completeButton',function(){
+      console.log("inside complete button on click");
+      completeTask();
+  });
+
   //on click delete calls deleteTask() and passes var $this to it
   deleteTask();
 }//ends eventListeners
